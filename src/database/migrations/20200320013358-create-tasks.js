cb52d8558511814fe8bc0b1489f1ce889f4c2d4f
burn-c <chopcsom@gmail.com>
2020-03-19 23:09:23 -0300

    Config: Migrations

---
+++ src/database/migrations/20200320013358-create-tasks.js
@@ -0,0 +1,64 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('taks', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: false
+      },
+      department_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'department', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: false
+      },
+      type_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'tasktype', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: false
+      },
+      status_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'taskstatus', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: false
+      },
+      start_date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      end_date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('tasks');
+  }
+};
